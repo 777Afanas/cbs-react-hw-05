439935d8ae34b6b8aa663d51394f48002dde02f0
Serhii Yatsenko <serhii2111@yahoo.com>
2024-10-25 17:59:06 +0300

    task_two

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [values, setValues] = useState([]);
+  const inpName = useRef();
+  const inpSurname = useRef();
+  const inpEmail  = useRef();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const onHandelClick = () => {
+    setValues([
+      ...values,
+      {
+        n: inpName.current.value,
+        s: inpSurname.current.value, 
+        e: inpEmail.current.value, 
+      },
+    ]);
+  };
+  
+  return (     
+      <div className="container">         
+        <p><label>Name:</label>
+          <input ref={inpName} type="text" /></p>
+        <p><label>Surname:</label>
+          <input ref={inpSurname} type="text" /></p>
+        <p><label>Email:</label>
+          <input ref={inpEmail} type="text" /></p> 
+
+        <button onClick={onHandelClick}>Push</button> 
+         
+        <ul>
+          {values.map((value, index) => (
+            <li key={index}>
+              <p>{value.n}</p>
+              <p>{value.s}</p>
+              <p>{value.e}</p>               
+            </li>
+          ))}
+        </ul>
+      </div>     
+  );
 }
 
-export default App
+export default App;
